f0529419d2a1967d3d09c56c58df6d351a572a4b
Rut Birhanu <birhanurut@gmail.com>
2025-03-28 15:38:57 +0300

    create route folders

--- frontend/src/components/SearchComponent.tsx
+++ frontend/src/components/SearchComponent.tsx
@@ -1,9 +1,16 @@
+import { Poppins } from "next/font/google"
+import { CiSearch } from "react-icons/ci"
+
+const poppins = Poppins({
+    weight:"200"
+})
+
 
 function SearchComponent() {
   return (
-      <div className="flex text-white bg-gray-900 rounded-xl px-3 py-1 mt-1 mb-10">
-          <span className="mr-3">Q</span>
-          <input type="text" placeholder="search here .." className="outline-none placeholder:text-[15px] placeholder:text-gray-300"/>
+      <div className="flex items-center text-white bg-gray-800 rounded-xl px-2 py-2 mt-1 mb-10">
+          <span className="mr-3"><CiSearch/></span>
+      <input type="text" placeholder="search here .." className={` outline-none text-[13px] placeholder:text-[13px] placeholder:text-gray-300 ${poppins.className} w-[100%]`} />
     </div>
   )
 }
