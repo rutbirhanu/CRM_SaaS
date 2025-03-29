3cfcd990fa7c03283b530dc3190aeada4e30c135
Rut Birhanu <birhanurut@gmail.com>
2025-03-29 13:47:02 +0300

    social media report cards

--- frontend/src/app/social_medias/page.tsx
+++ frontend/src/app/social_medias/page.tsx
@@ -1,8 +1,16 @@
+import SocialMediaComponent from '@/components/SocialMediaComponent'
 import React from 'react'
+import { FaFacebookF, FaInstagram, FaTiktok } from 'react-icons/fa'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div>
+    <div className='flex'>
+        <SocialMediaComponent icon={<FaFacebookF className="h-7 w-7 mb-2 rounded-sm bg-white text-black p-[3px]" />} mediaName='Facebook' followers='192,000' />
+        <SocialMediaComponent icon={<FaInstagram className="h-7 w-7 mb-2 rounded-sm bg-white text-black p-[3px]" />} mediaName='Instagram' followers='92,000' />
+        <SocialMediaComponent icon={<FaTiktok className="h-7 w-7 mb-2 rounded-sm p-[3px]" />} mediaName='TikTok' followers='320,000' />
+      </div>
+    </div>
   )
 }
 
--- frontend/src/components/SocialMediaComponent.tsx
+++ frontend/src/components/SocialMediaComponent.tsx
@@ -1,8 +1,32 @@
+import { ReactNode } from "react"
 
-function SocialMediaComponent() {
+interface SocialMediaComponentType{
+  icon: ReactNode,
+  mediaName: string,
+  followers: string,
+}
+
+
+const getBackgroundColor = (mediaName: string) => {
+  const colors: Record<string, string> = {
+    Facebook: "bg-blue-500 text-white", // Facebook Blue
+    Instagram: "bg-[linear-gradient(135deg,#f09433,#e6683c,#dc2743,#cc2366,#bc1888)] text-white", // Instagram Gradient
+    TikTok: "bg-black text-white", // TikTok Black
+    Twitter: "bg-blue-400", // Twitter Blue
+    YouTube: "bg-red-600", // YouTube Red
+  };
+
+  return colors[mediaName] || "bg-gray-200"; // Default background
+};
+
+function SocialMediaComponent({icon, mediaName, followers}: SocialMediaComponentType) {
   return (
-      <div>
-          
+    <div className={` py-5 px-9 m-2 flex flex-col shadow-2xl rounded-2xl w-fit  ${getBackgroundColor(mediaName)} `}>
+      <span >{icon}</span>
+      <p className="text-[15px] mb-4 font-media">{mediaName}</p>
+      <p className="font-medium text-[18px]">{followers}</p>
+      <p className="font-medium text-[13px]">Followers</p>
+
     </div>
   )
 }
