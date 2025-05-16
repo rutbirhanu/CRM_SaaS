697ab2e19a369af0e3451a925398fbb4e1c870e9
Rut Birhanu <birhanurut@gmail.com>
2025-05-16 20:33:31 +0300

    function to fetch follower count

--- frontend/src/redux/mediaSlice.ts
+++ frontend/src/redux/mediaSlice.ts
@@ -1,4 +1,4 @@
-import { createAsyncThunk } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 
 
 const BASEURL = "http://localhost:8080/socials"
@@ -7,7 +7,21 @@ export const fetchIGFollower = createAsyncThunk(
     "media/fetchIGFollower",
     async (_, { rejectWithValue }) => {
         try {
+            const res = await fetch(`${BASEURL}/instagram-stat`, {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            if (!res.ok) {
+                const errorData = await res.json();
+                return rejectWithValue(errorData.message || 'Failed to fetch analytics');
+            }
 
+            const data = await res.json();
+            // console.log(data)
+
+            return data;
         }
         catch (err: unknown) {
             if (err instanceof Error) {
@@ -17,3 +31,14 @@ export const fetchIGFollower = createAsyncThunk(
         }
     }
 )
+
+interface mediaState {
+    IGFollower : number,
+    loading: boolean,
+    error:boolean
+}
+const initialState:mediaState  = {
+    IGFollower : 0,
+    loading: false,
+    error:false
+}
